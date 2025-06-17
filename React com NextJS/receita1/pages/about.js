66180a76d7578c68e8020adfa08effcf55048672
Vitória Geovanna <vitoria31032003@gmail.com>
2025-06-16 22:54:25 -0300

    receita concluída

---
+++ React com NextJS/receita1/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function About(){
+    return (
+       <div> 
+          <h2>
+            Página sobre
+          </h2>
+       </div>
+    )
+}
---
+++ React com NextJS/receita1/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Contact(){
+    return (
+       <div>
+          <h2>
+            Página contato
+          </h2>
+       </div>
+    )
+}
